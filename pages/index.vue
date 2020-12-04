af431deefc1e6c54dc4f2bfbf884b39094a02074
Abel Esquivel <abelesquivel94@gmail.com>
2020-12-04 10:50:36 +0000

    firstCommit
    Co-authored-by: Eloy Pérez <eloypr@outlook.com>

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Página Principal</h1>
+    <v-btn color="primary" dark @click="goToCalendar()">Ir a Calendario</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  methods: {
+    goToCalendar() {
+      console.log("Entró");
+      this.$router.push({ name: "Calendario" });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
